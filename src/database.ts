b628a4d7e60533f2f4f538b5344ba2b664bcf453
Sayed Mahmoud <sayedmahmoudgomaa1@gmail.com>
2022-06-24 23:37:22 +0200

    POSTGRES_TEST_DB

--- src/database.ts
+++ src/database.ts
@@ -7,9 +7,9 @@ dotenv.config()
 const {
   POSTGRES_HOST,
   POSTGRES_DB,
+  POSTGRES_TEST_DB,
   POSTGRES_USER,
   POSTGRES_PASSWORD,
-  POSTGRES_TEST_DB,
   ENV /*,
   BCRYPT_PASSWORD,
   SALT_ROUNDS
@@ -22,14 +22,14 @@ console.log(`ENV: ${ENV}`)
 if (ENV === 'test') {
   client = new Pool({
     host: POSTGRES_HOST,
-    database: POSTGRES_DB,
+    database: POSTGRES_TEST_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD
   })
 } else if (ENV === 'dev') {
   client = new Pool({
     host: POSTGRES_HOST,
-    database: POSTGRES_TEST_DB,
+    database: POSTGRES_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD
   })
